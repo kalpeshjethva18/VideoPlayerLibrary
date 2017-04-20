abe9414ee8471a64d2825e0ffb0594501bba4535
kalpesh jethva <kalpeshjjethva@gmail.com>
2017-04-20 07:50:25 +0530

    first Commit

---
+++ InstagramVideoCell/ImageTableViewCell.swift
@@ -0,0 +1,25 @@
+//
+//  ImageTableViewCell.swift
+//  EditSoftDemo
+//
+//  Created by kalpesh jethva on 02/02/17.
+//  Copyright © 2017 kalpesh jethva. All rights reserved.
+//
+//sadf
+import UIKit
+
+class ImageTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var imageVIew: UIImageView!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ InstagramVideoCell/VideoPlayerView.swift
@@ -0,0 +1,22 @@
+//
+//  VideoPlayerView.swift
+//  EditSoftDemo
+//
+//  Created by kalpesh jethva on 01/02/17.
+//  Copyright © 2017 kalpesh jethva. All rights reserved.
+////sadf
+
+
+import UIKit
+
+class VideoPlayerView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
